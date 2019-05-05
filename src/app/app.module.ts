513f8245e0f762f4b368958c393556b0ff4700c4
notdmart <dylan.marty2@gmail.com>
2019-05-05 13:31:34 -0600

    Create results list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ResultService } from './core/result.service';
+import { ResultsListComponent } from './results-list/results-list.component';
 
 
 
 @NgModule({
     declarations: [
         AppComponent,
+        ResultsListComponent,
     ],
     imports: [
         BrowserModule,
